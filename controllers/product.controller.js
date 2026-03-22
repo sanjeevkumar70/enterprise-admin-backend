653764fbda1c12ed9de47c955c9555835d5ad849
prachi <prachimicrosoft99@gmail.com>
2026-03-22 20:42:50 +0530

    commited

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -71,7 +71,7 @@ exports.updateProduct = async (req, res) => {
     try {
         const { id } = req.params; // get id from params
         const product = await Product.findById(id);
-        
+
         if (!product) {
             return res.status(404).json({
                 success: false,
@@ -93,6 +93,34 @@ exports.updateProduct = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "Error updating product",
+            error: error.message
+        });
+    }
+};
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deletedProduct = await Product.findByIdAndDelete(id);
+
+        if (!deletedProduct) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Product deleted successfully",
+            data: deletedProduct
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error deleting product",
             error: error.message
         });
     }
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth.middleware");
-const { getProduct, updateProduct ,createProduct} = require("../controllers/product.controller");
+const { getProduct, updateProduct ,createProduct, deleteProduct} = require("../controllers/product.controller");
 
 router.get("/products", auth(["user", "admin", "manager"]), getProduct);
 router.post("/products", auth(["user", "admin", "manager"]), createProduct);
 router.put("/products/:id", auth(["user", "admin", "manager"]), updateProduct);
+router.delete("/products/:id", auth(["user", "admin", "manager"]), deleteProduct)
 
 module.exports = router;
